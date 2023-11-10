08a325cc90d586b6cd1211f7a32a668e584f1157
Monika <monika@theappbuildersgroup.com>
2023-11-09 21:13:37 -0500

    commit a1

---
+++ even_odd.js
@@ -0,0 +1,12 @@
+
+
+
+var sequence= [20,25,30,35,40,45];
+for (var i = 0; i < 7; i++) {
+   if ( i % 2 == 0 )
+    {
+        console.log(sequence[i]+ "-number is even." );
+    }
+   else{console.log(sequence[i]+"-number is odd.");}
+}
+
--- palindrome.js
+++ palindrome.js
@@ -8,8 +8,8 @@ function isPalindrome(str) {
     }
     return false
 }
-
 var str1 = "racecar";
 let str2 = "12321";
 console.log(isPalindrome(str1));
 console.log(isPalindrome(str2));
+//new branch changes-1
