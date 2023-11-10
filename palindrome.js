94f1d0a20ec2d13061b5d55924aa2616ac89b8f2
Monika <monika@theappbuildersgroup.com>
2023-11-09 20:17:46 -0500

    commit1

---
+++ palindrome.js
@@ -0,0 +1,15 @@
+
+
+function isPalindrome(str) {
+    var rev=str.split("").reverse().join("");
+
+    if(rev == str){
+        return true
+    }
+    return false
+}
+
+var str1 = "racecar";
+let str2 = "12321";
+console.log(isPalindrome(str1));
+console.log(isPalindrome(str2));
